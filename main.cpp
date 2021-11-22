6c40c605cdc44789dcfb1b75ff356172bf7106b4
MustBeAL <87888006+AlNotAlbert@users.noreply.github.com>
2021-11-22 02:53:10 -0600

    Simple Fizz Buzz program

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+void FizzBuzz(int num) {
+  if (num <= 2) {
+	cout << num << endl;
+	return;
+
+  }
+  for (int i = 0; i < num + 1; ++i) {
+	if (i % 3 == 0 && i % 5 == 0) {
+	  cout << "FizzBuzz(" << i << ")" << endl;
+
+	} else if (i % 3 == 0)
+	  std::cout << "Fizz(" << i << ")" << endl;
+	else if (i % 5 == 0) {
+	  std::cout << "Buzz(" << i << ")" << endl;
+	} else {
+	  cout << i << endl;
+	}
+  }
+  //cout << num << endl;
+}
+
+int main() {
+  cout << "Enter a number 1 - 100: " << endl;
+  short num_;
+  cin >> num_;
+  if (num_ > 100) {
+	cout << "Invalid Number" << endl;
+	exit(EXIT_FAILURE);
+
+  }
+  FizzBuzz(num_);
+  //Test("vi");
+  return 0;
+}
